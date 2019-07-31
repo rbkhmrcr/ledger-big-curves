2c320ba79180e3bacd90e173872c4b49b595cf04
Rebekah <github@rbkhmrcr.com>
2019-07-31 15:47:26 -0700

    finish is_zero fn

--- sign-app/src/arith/group-utils.c
+++ sign-app/src/arith/group-utils.c
@@ -1,16 +1,25 @@
 #include "group-utils.h"
 #include "field.h"
+#include <string.h>
 
-bool is_zero(gmnt6753 *p) { 
+bool is_zero(gmnt6753 *p) {
 
-  memcmp( p->X, fmnt6753_zero, fmnt6753_BYTES );
-  memcmp( p->Y, fmnt6753_one, fmnt6753_BYTES );
-  memcmp( p->Z, fmnt6753_zero, fmnt6753_BYTES );
+  if (memcmp(p->X, fmnt6753_zero, fmnt6753_BYTES) == 0 &&
+      memcmp(p->Y, fmnt6753_one, fmnt6753_BYTES) == 0 &&
+      memcmp(p->Z, fmnt6753_zero, fmnt6753_BYTES) == 0) {
+
+    return true;
+  }
+
+  return false;
 };
 
 bool is_on_curve(gmnt6753 *p) {
+
   if (is_zero(p)) {
+
     return true;
+
   } else if (fmnt6753_eq(p->Z, fmnt6753_one)) {
     // we can check y^2 == x^3 + ax + b
     fmnt6753 y2;
@@ -21,9 +30,9 @@ bool is_on_curve(gmnt6753 *p) {
     fmnt6753_add(x3_ax_b, x3_ax_b, gmnt6753_coeff_a); // x^2 + a
     fmnt6753_mul(x3_ax_b, x3_ax_b, p->X);             // x^3 + ax
     fmnt6753_add(x3_ax_b, x3_ax_b, gmnt6753_coeff_b); // x^3 + ax + b
-    
+
     return fmnt6753_eq(y2, x3_ax_b);
-  
+
   } else {
     // we check (y/z)^2 == (x/z)^3 + a(x/z) + b
     // => z(y^2 - bz^2) == x(x^2 + az^2)
@@ -33,15 +42,14 @@ bool is_on_curve(gmnt6753 *p) {
     fmnt6753_sq(z2, p->Z);
 
     fmnt6753 lhs, rhs;
-    fmnt6753_mul(lhs, z2, gmnt6753_coeff_b);      // bz^2
-    fmnt6753_sub(lhs, y2, lhs);                   // y^2 - bz^2
-    fmnt6753_mul(lhs, p->Z, lhs);                 // z(y^2 - bz^2)
-    fmnt6753_mul(rhs, z2, gmnt6753_coeff_a);      // az^2
-    fmnt6753_add(rhs, x2, rhs);                   // x^2 + az^2
-    fmnt6753_mul(rhs, p->X, rhs);                 // x(x^2 + az^2)
+    fmnt6753_mul(lhs, z2, gmnt6753_coeff_b); // bz^2
+    fmnt6753_sub(lhs, y2, lhs);              // y^2 - bz^2
+    fmnt6753_mul(lhs, p->Z, lhs);            // z(y^2 - bz^2)
+    fmnt6753_mul(rhs, z2, gmnt6753_coeff_a); // az^2
+    fmnt6753_add(rhs, x2, rhs);              // x^2 + az^2
+    fmnt6753_mul(rhs, p->X, rhs);            // x(x^2 + az^2)
 
     return fmnt6753_eq(lhs, rhs);
-
   }
 
   return false;
