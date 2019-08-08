407d200351111111d5edd49f9c4ba20133fbbe16
Rebekah <github@rbkhmrcr.com>
2019-08-07 19:24:22 -0700

    initial app commit

---
+++ sign-app/src/sign.h
@@ -0,0 +1,13 @@
+#ifndef CODA_SIGN
+#define CODA_SIGN
+
+#include "arith/field.h"
+#include "arith/group-utils.h"
+#include "arith/group.h"
+
+int sign(scalar6753 *pvkey, gmnt6753 *pbkey,
+         const unsigned char WIDE *hash PLENGTH(hash_len),
+         unsigned int hash_len, unsigned char *sig PLENGTH(sig_len),
+         unsigned int sig_len) {
+
+#endif // CODA_SIGN
