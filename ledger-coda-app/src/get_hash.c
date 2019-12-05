53eae6bc074c5bf83b0b264b204cb21f11efe417
Rebekah Mercer <github@rbkhmrcr.com>
2019-12-04 23:48:09 -0800

    txn hash changes

--- ledger-coda-app/src/get_hash.c
+++ ledger-coda-app/src/get_hash.c
@@ -37,6 +37,7 @@ static const bagl_element_t ui_hash_compare[] = {
   UI_TEXT(0x00, 0, 26, 128, global.h.partial_str),
 };
 
+// used with UX_DISPLAY, 'macro magic' :/
 static const bagl_element_t* ui_prepro_hash_compare(const bagl_element_t *element) {
   if ((element->component.userid == 1 && ctx->display_index == 0) ||
       (element->component.userid == 2 && ctx->display_index == ctx->elem_len-12)) {
@@ -106,24 +107,24 @@ static const bagl_element_t ui_hash_sign[] = {
 
 static unsigned int ui_hash_sign_button(unsigned int button_mask, unsigned int button_mask_counter) {
   switch (button_mask) {
-  // REJECT
-  case BUTTON_EVT_RELEASED | BUTTON_LEFT:
-    io_exchange_with_code(SW_USER_REJECTED, 0);
-    ui_idle();
-    break;
+    // REJECT
+    case BUTTON_EVT_RELEASED | BUTTON_LEFT:
+      io_exchange_with_code(SW_USER_REJECTED, 0);
+      ui_idle();
+      break;
 
-  // APPROVE
-  case BUTTON_EVT_RELEASED | BUTTON_RIGHT: {
-    scalar sk;
-    group pk;
-    generate_keypair(ctx->key_index, &pk, sk);
-    scalar hash = {0};
-    os_memmove(hash + 64, ctx->txn.hash, 32);
-    sign(G_io_apdu_buffer, G_io_apdu_buffer + field_bytes, &pk, sk, hash);
-    io_exchange_with_code(SW_OK, field_bytes + scalar_bytes);
-    ui_idle();
-    }
-    break;
+    // APPROVE
+    case BUTTON_EVT_RELEASED | BUTTON_RIGHT: {
+      scalar sk;
+      group pk;
+      generate_keypair(ctx->key_index, &pk, sk);
+      scalar hash = {0};
+      os_memmove(hash + 64, ctx->txn.hash, 32);
+      sign(G_io_apdu_buffer, G_io_apdu_buffer + field_bytes, &pk, sk, hash);
+      io_exchange_with_code(SW_OK, field_bytes + scalar_bytes);
+      ui_idle();
+      }
+      break;
   }
   return 0;
 }
@@ -341,113 +342,48 @@ void handle_hash(uint8_t p1, uint8_t p2, uint8_t *data_buffer, uint16_t data_len
   // code is essentially identical to ui_hash_elem_button. Sadly,
   // there doesn't seem to be a clean way to avoid this duplication.
   switch (txn_next_elem(&ctx->txn)) {
-  case TXN_STATE_ERR:
-    THROW(SW_INVALID_PARAM);
-  case TXN_STATE_PARTIAL:
-    THROW(SW_OK);
-  case TXN_STATE_READY:
-    ctx->elem_part = 0;
-    format_txn_elem(ctx);
-    UX_DISPLAY(ui_hash_elem, ui_prepro_hash_elem);
-    *flags |= IO_ASYNCH_REPLY;
-    break;
-  case TXN_STATE_FINISHED:
-    if (ctx->sign) {
-      os_memmove(ctx->full_str, "with Key #", 10);
-      bin2dec(ctx->full_str+10, ctx->key_index);
-      os_memmove(ctx->full_str+10+(bin2dec(ctx->full_str+10, ctx->key_index)), "?", 2);
-      UX_DISPLAY(ui_hash_sign, NULL);
+    case TXN_STATE_ERR:
+      THROW(SW_INVALID_PARAM);
+    case TXN_STATE_PARTIAL:
+      THROW(SW_OK);
+    case TXN_STATE_READY:
+      ctx->elem_part = 0;
+      format_txn_elem(ctx);
+      UX_DISPLAY(ui_hash_elem, ui_prepro_hash_elem);
       *flags |= IO_ASYNCH_REPLY;
-    } else {
-      os_memmove(G_io_apdu_buffer, ctx->txn.hash, 32);
-      io_exchange_with_code(SW_OK, 32);
-      bin2hex(ctx->full_str, ctx->txn.hash, sizeof(ctx->txn.hash));
-      os_memmove(ctx->partial_str, ctx->full_str, 12);
-      ctx->elem_len = 64;
-      ctx->display_index = 0;
-      UX_DISPLAY(ui_hash_compare, ui_prepro_hash_compare);
-      // The above code does something strange: it calls io_exchange
-      // directly from the command handler. You might wonder: why not
-      // just prepare the APDU buffer and let coda_main call io_exchange?
-      // The answer, surprisingly, is that we also need to call
-      // UX_DISPLAY, and UX_DISPLAY affects io_exchange in subtle ways.
-      // To understand why, we'll need to dive deep into the Nano S
-      // firmware. I recommend that you don't skip this section, even
-      // though it's lengthy, because it will save you a lot of
-      // frustration when you go "off the beaten path" in your own app.
-      //
-      // Recall that the Nano S has two chips. Your app (and the Ledger
-      // OS, BOLOS) runs on the Secure Element. The SE is completely
-      // self-contained; it doesn't talk to the outside world at all. It
-      // only talks to the other chip, the MCU. The MCU is what
-      // processes button presses, renders things on screen, and
-      // exchanges APDU packets with the computer. The communication
-      // layer between the SE and the MCU is called SEPROXYHAL. There
-      // are some nice diagrams in the "Hardware Architecture" section
-      // of Ledger's docs that will help you visualize all this.
-      //
-      // The SEPROXYHAL protocol, like any communication protocol,
-      // specifies exactly when each party is allowed to talk.
-      // Communication happens in a loop: first the MCU sends an Event,
-      // then the SE replies with zero or more Commands, and finally the
-      // SE sends a Status to indicate that it has finished processing
-      // the Event, completing one iteration:
-      //
-      //    Event -> Commands -> Status -> Event -> Commands -> ...
-      //
-      // For our purposes, an "Event" is a request APDU, and a "Command"
-      // is a response APDU. (There are other types of Events and
-      // Commands, such as button presses, but they aren't relevant
-      // here.) As for the Status, there is a "General" Status and a
-      // "Display" Status. A General Status tells the MCU to send the
-      // response APDU, and a Display Status tells it to render an
-      // element on the screen. Remember, it's "zero or more Commands,"
-      // so it's legal to send just a Status without any Commands.
-      //
-      // You may have some picture of the problem now. Imagine we
-      // prepare the APDU buffer, then call UX_DISPLAY, and then let
-      // coda_main send the APDU with io_exchange. What happens at the
-      // SEPROXYHAL layer? First, UX_DISPLAY will send a Display Status.
-      // Then, io_exchange will send a Command and a General Status. But
-      // no Event was processed between the two Statuses! This causes
-      // SEPROXYHAL to freak out and crash, forcing you to reboot your
-      // Nano S.
-      //
-      // So why does calling io_exchange before UX_DISPLAY fix the
-      // problem? Won't we just end up sending two Statuses again? The
-      // secret is that io_exchange_with_code uses the
-      // IO_RETURN_AFTER_TX flag. Previously, the only thing we needed
-      // to know about IO_RETURN_AFTER_TX is that it sends a response
-      // APDU without waiting for the next request APDU. But it has one
-      // other important property: it tells io_exchange not to send a
-      // Status! So the only Status we send comes from UX_DISPLAY. This
-      // preserves the ordering required by SEPROXYHAL.
-      //
-      // Lastly: what if we prepare the APDU buffer in the handler, but
-      // with the IO_RETURN_AFTER_TX flag set? Will that work?
-      // Unfortunately not. io_exchange won't send a status, but it
-      // *will* send a Command containing the APDU, so we still end up
-      // breaking the correct SEPROXYHAL ordering.
-      //
-      // Here's a list of rules that will help you debug similar issues:
-      //
-      // - Always preserve the order: Event -> Commands -> Status
-      // - UX_DISPLAY sends a Status
-      // - io_exchange sends a Command and a Status
-      // - IO_RETURN_AFTER_TX makes io_exchange not send a Status
-      // - IO_ASYNCH_REPLY (or tx=0) makes io_exchange not send a Command
-      //
-      // Okay, that second rule isn't 100% accurate. UX_DISPLAY doesn't
-      // necessarily send a single Status: it sends a separate Status
-      // for each element you render! The reason this works is that the
-      // MCU replies to each Display Status with a Display Processed
-      // Event. That means you can call UX_DISPLAY many times in a row
-      // without disrupting SEPROXYHAL. Anyway, as far as we're
-      // concerned, it's simpler to think of UX_DISPLAY as sending just
-      // a single Status.
+      break;
+    case TXN_STATE_FINISHED:
+      if (ctx->sign) {
+        os_memmove(ctx->full_str, "with Key #", 10);
+        bin2dec(ctx->full_str+10, ctx->key_index);
+        os_memmove(ctx->full_str+10+(bin2dec(ctx->full_str+10, ctx->key_index)), "?", 2);
+        UX_DISPLAY(ui_hash_sign, NULL);
+        *flags |= IO_ASYNCH_REPLY;
+      } else {
+        os_memmove(G_io_apdu_buffer, ctx->txn.hash, 32);
+        io_exchange_with_code(SW_OK, 32);
+        bin2hex(ctx->full_str, ctx->txn.hash, sizeof(ctx->txn.hash));
+        os_memmove(ctx->partial_str, ctx->full_str, 12);
+        ctx->elem_len = 64;
+        ctx->display_index = 0;
+        UX_DISPLAY(ui_hash_compare, ui_prepro_hash_compare);
+        // - Always preserve the order: Event -> Commands -> Status
+        // - UX_DISPLAY sends a Status
+        // - io_exchange sends a Command and a Status
+        // - IO_RETURN_AFTER_TX makes io_exchange not send a Status
+        // - IO_ASYNCH_REPLY (or tx=0) makes io_exchange not send a Command
+        //
+        // Okay, that second rule isn't 100% accurate. UX_DISPLAY doesn't
+        // necessarily send a single Status: it sends a separate Status
+        // for each element you render! The reason this works is that the
+        // MCU replies to each Display Status with a Display Processed
+        // Event. That means you can call UX_DISPLAY many times in a row
+        // without disrupting SEPROXYHAL. Anyway, as far as we're
+        // concerned, it's simpler to think of UX_DISPLAY as sending just
+        // a single Status.
+      }
+      // Reset the initialization state.
+      ctx->initialized = 0;
+      break;
     }
-    // Reset the initialization state.
-    ctx->initialized = 0;
-    break;
-  }
 }
